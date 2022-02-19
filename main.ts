a7ac705998f7e97d8068b8078d2ba2e08401981f
zabihollah11 <100030382+zabihollah11@users.noreply.github.com>
2022-02-19 14:35:33 +0100

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,68 @@
+let running = false
+let false_start = false
+let start = 0
+let end = 0
+input.onPinPressed(TouchPin.P0, function () {
+    basic.showNumber(3)
+    basic.showNumber(2)
+    basic.showNumber(1)
+    basic.clearScreen()
+    running = false
+    false_start = false
+    basic.pause(1000 + randint(0, 1999))
+    if (!(false_start)) {
+        start = input.runningTime()
+        running = true
+        led.stopAnimation()
+        basic.clearScreen()
+        led.plot(randint(0, 4), randint(0, 4))
+    }
+})
+input.onPinPressed(TouchPin.P2, function () {
+    if (running) {
+        running = false
+        end = input.runningTime()
+        basic.showLeds(`
+            . . . # #
+            . . . # #
+            . . . # #
+            . . . # #
+            . . . # #
+            `)
+        basic.pause(1000)
+        basic.showNumber(end - start)
+    } else {
+        false_start = true
+        basic.showLeds(`
+            . . . . .
+            . . # . #
+            . . . # .
+            . . # . #
+            . . . . .
+            `)
+    }
+})
+input.onPinPressed(TouchPin.P1, function () {
+    if (running) {
+        running = false
+        end = input.runningTime()
+        basic.showLeds(`
+            # # . . .
+            # # . . .
+            # # . . .
+            # # . . .
+            # # . . .
+            `)
+        basic.pause(1000)
+        basic.showNumber(end - start)
+    } else {
+        false_start = true
+        basic.showLeds(`
+            . . . . .
+            # . # . .
+            . # . . .
+            # . # . .
+            . . . . .
+            `)
+    }
+})
